0d9c5ff6db8205a1fa95d6d371cf8692a12addd1
Olga Semenova <helen597597@gmail.com>
2024-01-14 18:43:02 +0500

    add functions save_data_to_database, print_employers

--- src/functions.py
+++ src/functions.py
@@ -1,78 +1,84 @@
-import json, os
-from src.vacancy import Vacancy
-from src.JSON_processor import JSONSaver
 from typing import Any
 from datetime import date
 # import psycopg2
 
 
-def filter_vacancies(hh_vacancies, filter_words):
-    """Функция, фильтрующая вакансии с сайтов hh и superjob"""
-    new_list = []
-    filter_words = set(filter_words)
-    punc = """!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
+# def filter_vacancies(hh_vacancies, filter_words):
+#     """Функция, фильтрующая вакансии с сайтов hh и superjob"""
+#     new_list = []
+#     filter_words = set(filter_words)
+#     punc = """!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
+#
+#     if os.path.exists('filtered.json'):
+#         with open('filtered.json', 'w', encoding='utf-8') as f:
+#             json.dump(new_list, f, ensure_ascii=False)
+#
+#     with open(hh_vacancies, 'r', encoding='utf-8') as f:
+#         content = json.load(f)
+#
+#     # Цикл по вакансиям hh
+#     for vacancy in content:
+#
+#         requirements = vacancy['requirements']
+#         if requirements is None:
+#             requirements = set()
+#         else:
+#             for s in requirements:
+#                 if s in punc:
+#                     new_str = requirements.replace(s, "")
+#             requirements = set(new_str.lower().split())
+#
+#         description = vacancy['description']
+#         if description is None:
+#             description = set()
+#         else:
+#             for s in description:
+#                 if s in punc:
+#                     new_str = description.replace(s, "")
+#             description = set(new_str.lower().split())
+#
+#         if filter_words.intersection(requirements.union(description)) == filter_words:
+#             new_vacancy = Vacancy(vacancy['name'], vacancy['url'], vacancy['salary'],
+#                                     vacancy['requirements'],vacancy['employer'], vacancy['description'])
+#             new_list.append(new_vacancy)
+#             JSONSaver.add_vacancy('filtered.json', new_vacancy)
+#
+#     return new_list
+#
+#
+# def sort_by_salary(vacancy):
+#     """Функция ключа сортировки"""
+#     if vacancy.salary:
+#         if vacancy.salary['from']:
+#             return vacancy.salary['from']
+#     return 0
+#
+#
+# def sort_vacancies(filtered_vacancies):
+#     """Функция, сортирующая вакансии по зарплате от большей к меньшей"""
+#     filtered_vacancies.sort(key=sort_by_salary, reverse=True)
+#     return filtered_vacancies
+#
+#
+# def get_top_vacancies(sorted_vacancies, top_n):
+#     """Функция, возвращающая N первых вакансий"""
+#     return sorted_vacancies[:top_n]
+#
+#
+# def print_vacancies(vacancies):
+#     """Функция печати списка вакансий"""
+#     for i in range(len(vacancies)):
+#         print(i+1)
+#         print(vacancies[i])
 
-    if os.path.exists('filtered.json'):
-        with open('filtered.json', 'w', encoding='utf-8') as f:
-            json.dump(new_list, f, ensure_ascii=False)
 
-    with open(hh_vacancies, 'r', encoding='utf-8') as f:
-        content = json.load(f)
-
-    # Цикл по вакансиям hh
-    for vacancy in content:
-
-        requirements = vacancy['requirements']
-        if requirements is None:
-            requirements = set()
-        else:
-            for s in requirements:
-                if s in punc:
-                    new_str = requirements.replace(s, "")
-            requirements = set(new_str.lower().split())
-
-        description = vacancy['description']
-        if description is None:
-            description = set()
-        else:
-            for s in description:
-                if s in punc:
-                    new_str = description.replace(s, "")
-            description = set(new_str.lower().split())
-
-        if filter_words.intersection(requirements.union(description)) == filter_words:
-            new_vacancy = Vacancy(vacancy['name'], vacancy['url'], vacancy['salary'],
-                                    vacancy['requirements'],vacancy['employer'], vacancy['description'])
-            new_list.append(new_vacancy)
-            JSONSaver.add_vacancy('filtered.json', new_vacancy)
-
-    return new_list
-
-
-def sort_by_salary(vacancy):
-    """Функция ключа сортировки"""
-    if vacancy.salary:
-        if vacancy.salary['from']:
-            return vacancy.salary['from']
-    return 0
-
-
-def sort_vacancies(filtered_vacancies):
-    """Функция, сортирующая вакансии по зарплате от большей к меньшей"""
-    filtered_vacancies.sort(key=sort_by_salary, reverse=True)
-    return filtered_vacancies
-
-
-def get_top_vacancies(sorted_vacancies, top_n):
-    """Функция, возвращающая N первых вакансий"""
-    return sorted_vacancies[:top_n]
-
-
-def print_vacancies(vacancies):
-    """Функция печати списка вакансий"""
-    for i in range(len(vacancies)):
-        print(i+1)
-        print(vacancies[i])
+def print_employers(data) -> None:
+    for employer in data['items']:
+        employer_id = employer['id']
+        employer_name = employer['name']
+        # industries = employer['industries']
+        # vacancies_count = employer['open_vacancies']
+        print(f"{employer_id} - {employer_name}")
 
 
 def get_data(hh_employers, hh_vacancies, employers_id_list: list[int]) -> list[dict[str, Any]]:
@@ -133,4 +139,41 @@ def create_database(database_name: str, params: dict) -> None:
 
 def save_data_to_database(database_name: str, data: list[dict[str, Any]], params: dict) -> None:
     """Сохранение данных в базу данных"""
-    pass
+    conn = psycopg2.connect(dbname=database_name, **params)
+    try:
+        with conn:
+            with conn.cursor() as cur:
+                for dictionary in data:
+                    employer_data = dictionary['employer']
+                    cur.execute(
+                        """
+                        INSERT INTO employers(employer_id, name, vacancies_count)
+                        VALUES (%s, %s, %s)
+                        """,
+                        (employer_data['id'],
+                         employer_data['name'],
+                         employer_data['open_vacancies'])
+                    )
+                    vacancies_data = dictionary['vacancies']
+                    for vacancy in vacancies_data:
+                        cur.execute(
+                            """
+                            INSERT INTO vacancies
+                            (title, employer_id, publish_date, url, 
+                            salary_from, salary_to, salary_currency, salary_gross,
+                            requirements, description)
+                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                            """,
+                            (vacancy['name'],
+                             vacancy['employer']['id'],
+                             date(vacancy['published_at']),
+                             vacancy['alternate_url'],
+                             vacancy['salary']['from'],
+                             vacancy['salary']['to'],
+                             vacancy['salary']['currency'],
+                             vacancy['salary']['gross'],
+                             vacancy['snippet']['requirement'],
+                             vacancy['snippet']['responsibility'])
+                        )
+    finally:
+        conn.close()
